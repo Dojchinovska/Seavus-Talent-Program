5d4b8733d810e2df28ef65195fc8ced44607ddd7
Dojchinovska <dojchinovskaa@gmail.com>
2020-09-15 19:37:20 +0200

    Renamed class

---
+++ DateParser_/src/com/company/DateParser.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.*;
+
+public class DateParser {
+    public static void main(String[] args) {
+        System.out.print("Enter date in MM/DD/YYYY format");
+        String scn = new Scanner(System.in).nextLine();
+
+        String month = scn.substring(0, 2);
+        String day = scn.substring(3, 5);
+        String year = scn.substring(6, 10);
+
+        System.out.println("Month: " + month + " Day: " + day + " Year: " + year);
+
+    }
+}
