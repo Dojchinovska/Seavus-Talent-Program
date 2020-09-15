4d4aff4649ef46a2dc98e08cef5613a7c0a1c72c
Dojchinovska <dojchinovskaa@gmail.com>
2020-09-15 22:36:44 +0200

    Code change and reformat

--- CharCounter/src/com/company/Application.java
+++ CharCounter/src/com/company/Application.java
@@ -8,17 +8,15 @@ public class Application {
         String sentence = new Scanner(System.in).nextLine();
         char[] content = sentence.toCharArray();
 
-        HashMap<Character, Integer> mapa = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> m = new HashMap<Character, Integer>();
 
-        for (char ch : content) {
-            if (mapa.containsKey(ch)) {
-                mapa.put(ch, mapa.get(ch) + 1);
-            } else {
-                mapa.put(ch, 1);
-            }
+        for (char c : content) {
+
+            m.put(c, (m.containsKey(c) ? m.get(c) + 1 : 1));
         }
 
-        for (HashMap.Entry<Character, Integer> val : mapa.entrySet()) {
+
+        for (HashMap.Entry<Character, Integer> val : m.entrySet()) {
             System.out.println("Character: " + val.getKey() + "  Number of occurrences: " + val.getValue().toString());
         }
 
