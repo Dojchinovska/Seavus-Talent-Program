e1976deab9601bce31dd1beb7f2f23feb2d959a1
Dojchinovska <dojchinovskaa@gmail.com>
2020-09-16 00:52:02 +0200

    Changed class name

--- Address/src/com/company/Application.java
+++ Address/src/com/company/Application.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.*;
 
-public class Main {
+public class Application {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
