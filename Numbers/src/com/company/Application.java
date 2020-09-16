fdf7c6f60806a168a0d67e5c1e50b7199441bfdf
Dojchinovska <dojchinovskaa@gmail.com>
2020-09-16 17:22:29 +0200

    Code reformat

--- Numbers/src/com/company/Application.java
+++ Numbers/src/com/company/Application.java
@@ -11,53 +11,41 @@ public class Application {
 
         for (String x : result) {
             int value = 0;
-
             switch (x) {
                 case "one":
                     value = 1;
                     break;
-
                 case "two":
                     value = 2;
                     break;
-
                 case "three":
                     value = 3;
                     break;
-
                 case "four":
                     value = 4;
                     break;
-
                 case "five":
                     value = 5;
                     break;
-
                 case "six":
                     value = 6;
                     break;
-
                 case "seven":
                     value = 7;
                     break;
-
                 case "eight":
                     value = 8;
                     break;
-
                 case "nine":
                     value = 9;
                     break;
-
                 case "zero":
                     value = 0;
                     break;
-
                 default:
                     System.out.println("Invalid input");
                     break;
             }
-
             System.out.print(value);
         }
     }
