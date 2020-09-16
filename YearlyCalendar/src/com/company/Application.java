c99fae25a631b7b27667be347d1badb0ef55d9bb
Dojchinovska <dojchinovskaa@gmail.com>
2020-09-16 12:44:33 +0200

    Switch case Java 8

--- YearlyCalendar/src/com/company/Application.java
+++ YearlyCalendar/src/com/company/Application.java
@@ -21,12 +21,20 @@ public class Application {
 
             switch (month) {
 
-                case 1, 3, 5, 7, 8, 10, 12:
+                case 1:
+                case 3:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
 
                     days = 31;
                     break;
 
-                case 4, 6, 9, 11:
+                case 4:
+                case 6:
+                case 9:
+                case 11:
 
                     days = 30;
                     break;
