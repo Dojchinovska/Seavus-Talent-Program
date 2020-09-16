f893a1ab5e1714ced250145d06fea07b2d3d7715
Dojchinovska <dojchinovskaa@gmail.com>
2020-09-16 17:36:43 +0200

    Code reformat

--- CharCounter/src/com/company/Application.java
+++ CharCounter/src/com/company/Application.java
@@ -3,22 +3,16 @@ package com.company;
 import java.util.*;
 
 public class Application {
-
     public static void main(String[] args) {
         String sentence = new Scanner(System.in).nextLine();
         char[] content = sentence.toCharArray();
-
         HashMap<Character, Integer> m = new HashMap<Character, Integer>();
 
         for (char c : content) {
-
             m.put(c, (m.containsKey(c) ? m.get(c) + 1 : 1));
         }
-
-
         for (HashMap.Entry<Character, Integer> val : m.entrySet()) {
             System.out.println("Character: " + val.getKey() + "  Number of occurrences: " + val.getValue().toString());
         }
-
     }
 }
