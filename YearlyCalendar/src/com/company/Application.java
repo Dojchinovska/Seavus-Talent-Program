0cd6b63e1468d124ea712b925ea2c3fca3b4ada1
Dojchinovska <dojchinovskaa@gmail.com>
2020-09-16 17:24:35 +0200

    Code reformat

--- YearlyCalendar/src/com/company/Application.java
+++ YearlyCalendar/src/com/company/Application.java
@@ -18,40 +18,31 @@ public class Application {
 
         for (int month = 1; month <= 12; month++) {
             int days = 0;
-
             switch (month) {
-
                 case 1:
                 case 3:
                 case 7:
                 case 8:
                 case 10:
                 case 12:
-
                     days = 31;
                     break;
-
                 case 4:
                 case 6:
                 case 9:
                 case 11:
-
                     days = 30;
                     break;
-
                 case 2:
-
                     if (leapFlag) {
                         days = 29;
                     } else {
                         days = 28;
                     }
                     break;
-
             }
 
             for (int day = 1; day <= days; day++) {
-
                 String date = day + "-" + month + "-" + year;
                 dates = new String[sumdays];
 
